4d6628da7686bf7d07d886fea9b50629255b3f21
nziokikevin <kevinnzioki10@gmail.com>
2023-04-25 13:02:36 +0300

    :pencil2: typo: correcting a typing error

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -29,7 +29,7 @@ function AddContact(){
             company: contact.company,
             group: contact.group
         }
-        fetch("https://json-server-api-e1284-default-rtdb.europe-west1.firebasedatabase.app/contactss", {
+        fetch("https://json-server-api-e1284-default-rtdb.europe-west1.firebasedatabase.app/contacts", {
             method: 'POST',
             headers:{
                 "Content-Type": "application/json",
